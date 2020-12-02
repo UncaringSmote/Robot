4063d3191a3e955d61c9ec98939f77d3f0b687c5
Middleton, Douglas Jay <middleton.douglas@gatech.edu>
2020-12-01 21:34:39 -0500

    better start point

--- Map/map.py
+++ Map/map.py
@@ -3,6 +3,6 @@ from geopy import distance
 from path import Path
 
 # given: lat1, lon1, b = bearing in degrees, d = distance in kilometers
-initial_lat_lon=(30.39245978916698, -84.28468018371238)
-initial_brn = 0
+initial_lat_lon=(30.5183237847436, -84.2486411333084)
+initial_brn = 90
 Path(initial_lat_lon,initial_brn)
